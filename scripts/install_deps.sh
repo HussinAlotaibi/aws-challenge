044b8940e88e28bba371c5506b67d52eaaf71b90
HussinAlotaibi <t8881@hotmail.com>
2026-05-17 13:43:54 +0300

    fix: install MySQL dev headers before pip install

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 cd /opt/app
+dnf install -y python3-devel mysql-devel gcc pkg-config
 pip3 install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
