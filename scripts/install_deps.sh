67a7bbb8ae4c4027b42848c90c31527c94edfef3
HussinAlotaibi <t8881@hotmail.com>
2026-05-17 14:08:16 +0300

    fix: use cloud-init status --wait instead of polling loop

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-# Wait for user_data to finish creating env file (up to 5 mins)
-for i in {1..30}; do
-  [ -f /etc/profile.d/app.sh ] && break
-  echo "Waiting for app.sh... attempt $i"
-  sleep 10
-done
+# Wait for cloud-init (user_data) to fully complete
+cloud-init status --wait
+
 source /etc/profile.d/app.sh
 
 cd /opt/app
